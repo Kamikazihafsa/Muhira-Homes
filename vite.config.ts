84a5605a507b2671bbd3bdbfefc3fbae2bd12fb0
kamik <kamikazihafsa2@gmail.com>
2025-09-14 21:27:46 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Muhira-Homes/',   // 👈 IMPORTANT for GitHub Pages
+  base: '/Muhira-Homes/',   // must match repo name & casing
 })
